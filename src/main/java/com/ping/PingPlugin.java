f327a9fc24ae9705f9a087f097cef38c6667a58e
uwuawa_ <kai90258@gmail.com>
2025-05-30 13:58:55 +0800

    Latest

---
+++ src/main/java/com/ping/PingPlugin.java
@@ -0,0 +1,61 @@
+package com.ping;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getCommand("ping").setExecutor(this);
+        getLogger().info("PingPlugin 已啟動！");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "這個指令只能由玩家執行。");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            int ping = getPing(player);
+            String pingColor = getPingColor(ping);
+            String message = "&8[&ePing&8] &7" + player.getName() + " " + pingColor + ping + " &8ms.";
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        } else {
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target == null || !target.isOnline()) {
+                player.sendMessage(ChatColor.RED + "找不到玩家 " + args[0]);
+                return true;
+            }
+
+            int ping = getPing(target);
+            String pingColor = getPingColor(ping);
+            String message = "&8[&ePing&8] &7" + target.getName() + " " + pingColor + ping + " &8ms.";
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
+
+        return true;
+    }
+
+    private int getPing(Player player) {
+        return player.getPing(); // ✅ 使用官方 API，無需反射
+    }
+
+    private String getPingColor(int ping) {
+        if (ping <= 120) {
+            return "&a"; // 綠色
+        } else if (ping <= 180) {
+            return "&e"; // 黃色
+        } else {
+            return "&c"; // 紅色
+        }
+    }
+}
